d67880eddf0e3733e4755f9848a6f4c7539b74c3
daniel-efrat <daniel989@gmail.com>
2023-02-07 17:25:58 +0100

    firebase 2

--- firebase/index.js
+++ firebase/index.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMxM4Rt45KGeb121rUOENBu3Jsqdu0L_I",
-  authDomain: "next-a500b.firebaseapp.com",
-  projectId: "next-a500b",
-  storageBucket: "next-a500b.appspot.com",
-  messagingSenderId: "168712146976",
-  appId: "1:168712146976:web:3190dee060b8836166ec45",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig);
