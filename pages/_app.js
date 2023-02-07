afd34daea6fb1ad3c7c078f3d40f03ba06364331
daniel-efrat <daniel989@gmail.com>
2023-02-07 05:43:10 +0100

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,13 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react"
 import "../styles/globals.css"
-import dotenv from "dotenv"
 
-dotenv.config()
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
